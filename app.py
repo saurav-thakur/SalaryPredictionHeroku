2a474a50142e44c0d832a8478baf4b4ebc2d9e24
saurav-thakur <46792248+saurav-thakur@users.noreply.github.com>
2020-10-21 00:04:16 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,29 @@
+import numpy as np
+from flask import Flask, request, render_template
+import pickle
+
+app = Flask(__name__)
+# model = pickle.load(open('model.pkl'), 'rb')
+with open('model.pkl', 'rb') as f:
+    # load using pickle de-serializer
+    model = pickle.load(f)
+
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    int_features = [int(x) for x in request.form.values()]
+    final_features = [np.array(int_features)]
+    prediction = model.predict(final_features)
+
+    output = round(prediction[0], 2)
+
+    return render_template('index.html', prediction_text='The Predicted Salary is  $ {}'.format(output))
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
